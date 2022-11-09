fcbe308dd0cfdcdf41e9e914e2de7949ea7129bc
stephen shadrach <stephenlgz@gmail.com>
2022-11-08 23:10:17 -0500

    posts

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const firebase = require("firebase/app");
+// initiate express
 const app = express();
+// setting port - dynamically with heroku
 const port = process.env.PORT || 4000;
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCA4TygMEysbLTgOvjKd-m3eqQhG5HO3i0",
+  authDomain: "dynamic-web-exercise-five-2022.firebaseapp.com",
+  projectId: "dynamic-web-exercise-five-2022",
+  storageBucket: "dynamic-web-exercise-five-2022.appspot.com",
+  messagingSenderId: "247044724219",
+  appId: "1:247044724219:web:62e72e671b7f256e48275e",
+};
+firebase.initializeApp(firebaseConfig);
 
+// routes for directing user to correct place
 const indexRoute = require("./routes/index");
+const singlePostRoute = require("./routes/singlePost");
+const createPostRoute = require("./routes/createPost");
 
+// tell express to use routes...
 app.use("/", indexRoute);
+app.use("/post", singlePostRoute);
+app.use("/create", createPostRoute);
 
+// listen for requests on the port
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const router = express.Router();
+// initialize firestore
+const firestore = require("firebase/firestore");
+//create a reference to the database
+const db = firestore.getFirestore();
 
+// middleware that is specific to this router
 router.use((req, res, next) => {
   console.log("Time: ", Date.now());
   next();
 });
-
+// define the home page route
 router.get("/", (req, res) => {
-  res.send("Home!");
+  const postsQuery = firestore.getDocs(firestore.collection(db, "posts"));
+  const postsArray = [];
+
+  postsQuery
+    .then((response) => {
+      response.forEach((post) => {
+        console.log(post.data());
+        postsArray.push({ id: post.id, ...post.data() });
+      });
+      res.send(postsArray);
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.send(error);
+    });
 });
 
 module.exports = router;
